5f3d55e3faf20079778eae4bbb75fa2b00b00140
als-menezes <andre.sousamenezes2@gmail.com>
2026-06-27 11:46:14 -0300

    >> StartRecipeBTN text swipe & localstorage

--- src/Components/StartRecipeBTN.js
+++ src/Components/StartRecipeBTN.js
@@ -4,7 +4,42 @@ import { useHistory } from 'react-router-dom';
 function StartRecipeButton({ recipeId, type }) {
   const history = useHistory();
 
+  const storageKey = type === 'meal' ? 'meals' : 'drinks';
+
+  const doneRecipes = JSON.parse(localStorage.getItem('doneRecipes')) || [];
+
+  const inProgressRecipes = JSON.parse(
+    localStorage.getItem('inProgressRecipes'),
+  ) || {
+    meals: {},
+    drinks: {},
+  };
+
+  const isRecipeDone = doneRecipes.some(
+    (recipe) => recipe.id === recipeId && recipe.type === type,
+  );
+
+  const isRecipeInProgress = Boolean(
+    inProgressRecipes[storageKey]
+      && inProgressRecipes[storageKey][recipeId],
+  );
+
   const handleStartRecipe = () => {
+    if (!isRecipeInProgress) {
+      const updatedInProgressRecipes = {
+        ...inProgressRecipes,
+        [storageKey]: {
+          ...inProgressRecipes[storageKey],
+          [recipeId]: [],
+        },
+      };
+
+      localStorage.setItem(
+        'inProgressRecipes',
+        JSON.stringify(updatedInProgressRecipes),
+      );
+    }
+
     history.push(`/${type}s/${recipeId}/in-progress`);
   };
 
@@ -21,13 +56,15 @@ function StartRecipeButton({ recipeId, type }) {
     cursor: 'pointer',
   };
 
+  if (isRecipeDone) return null;
+
   return (
     <button
       type="button"
       style={ buttonStyle }
       onClick={ handleStartRecipe }
     >
-      Start Recipe
+      {isRecipeInProgress ? 'Continue Recipe' : 'Start Recipe'}
     </button>
   );
 }
